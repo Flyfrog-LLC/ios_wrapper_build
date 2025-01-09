b2ef84cfbf363e6d8cc78b54f83b8b456c94b144
_vik <golubenkovvs@gmail.com>
2025-01-09 21:35:59 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "LibXray",
-            url: "https://github.com/Flyfrog-LLC/ios_wrapper/releases/download/v3.1.24/LibXray.xcframework.zip",
+            url: "https://github.com/Flyfrog-LLC/ios_wrapper_build/releases/download/v3.1.24/LibXray.xcframework.zip",
             checksum: "31ed6df9e4af57552ad6844d95f18bb5f238ef139f05e0efa5de8b83bc60d7bd"
         )
     ]
