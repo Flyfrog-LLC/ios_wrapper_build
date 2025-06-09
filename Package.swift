21d47f230dfed0698a1c39771b7b29da0a81263a
InfiniteFalltrough <golubenkovvs@gmail.com>
2025-06-09 11:14:51 +0700

    new checksum/new binary

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "LibXray",
             url: "https://github.com/Flyfrog-LLC/ios_wrapper_build/releases/download/v3.6.0/LibXray.xcframework.zip",
-            checksum: "d66632b764d6dc8d41a1b17cd1fb62798d6740c4f45e06181832be367c7be85c"
+            checksum: "1ccbc599d3821fb23452a7a22170024efc489e2c12ebb5d42d1d8f621c8417a2"
         )
     ]
 )
